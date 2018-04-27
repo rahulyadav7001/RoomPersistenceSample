c24efe83b891c52a16c918f96605abd1f3ab4c25
rahulyadav7001 <rahulyadav7001@gmail.com>
2018-04-28 02:11:44 +0530

    Initial commit.

--- app/src/main/java/com/ryandro/room_persistence_sample/MainActivity.java
+++ app/src/main/java/com/ryandro/room_persistence_sample/MainActivity.java
@@ -1,23 +1,30 @@
 package com.ryandro.room_persistence_sample;
 
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.ryandro.room_persistence_sample.database.AppDataBase;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn_test = findViewById(R.id.btn_test);
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatabaseInitializer.PopulateDbAsync(AppDataBase.getAppDataBase(MainActivity.this));
+            }
+        });
 
-
-    /*    ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        activityMainBinding.clickHereBtn.setOnClickListener(view ->
-                DatabaseInitializer.PopulateDbAsync(AppDataBase.getAppDataBase(this))
-        );*/
+//        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        activityMainBinding.clickHereBtn.setOnClickListener(view ->
+//                DatabaseInitializer.PopulateDbAsync(AppDataBase.getAppDataBase(this))
+//        );
     }
 }
